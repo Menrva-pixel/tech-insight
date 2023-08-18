2ac49bcf11960b7f132d7cfe6a63e37a5b30ba08
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-08-18 21:18:40 +0700

    merapihkan footer dan navbar

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,8 @@
         <div class="flex flex-row justify-between bg-[#464646] text-white p-10">
             <div class="flex flex-col">
                 <h2 class="font-semibold text-2xl mb-4">Tentang Website</h2>
-                <p>awalnya saya hanya coba-coba, eh kok lama-lama ketagihan,<br>btw bokong loba gede banget ajg gua liat hentainya kemaren.</p>
-                <p class="text-end">- Van</p>
+                <p>Website ini dibuat untuk seputar informasi tentang teknologi</p>
+                <p class="text-end">- Developer</p>
             </div>
             <div class="flex flex-col">
                 <h2 class="font-semibold text-2xl mb-4">Social Media</h2>
@@ -15,9 +15,9 @@
             </div>
             <div class="flex flex-col">
                 <h2 class="font-semibold text-2xl mb-4">Kontak Informasi</h2>
-                <span>Alamat: kota yang paling panas di jawa barat</span>
+                <span>Alamat: Indonesia</span>
                 <span>0811-1111-1111</span>
-                <span>suck.madik@gaymail.com</span>
+                <span>test@test.com</span>
             </div>
         </div>
         <div class="mx-auto text-center py-4 bg-[#212121] text-white">
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="hero-section" class="banner">
         <div class="banner-title flex flex-col items-start xl:pl-60 p-4">
-            <h1 class="text-7xl my-20 font-bold text-blue-300">Welcome</h1>
+            <h1 class="text-7xl font-bold text-blue-300 mt-28 mb-20">Welcome</h1>
             <h3 class="text-2xl font-semibold text-blue-700">Selamat datang di website kami</h3>
-            <a href="" class="p-4 border-2 rounded-md mt-4 hover:bg-red-400 duration-300 hover:text-black">TOMBOL</a>
-        </div>
+            <a href="" class="p-4 border-2 rounded-md mt-5 hover:bg-red-400 duration-300 hover:text-black">TOMBOL</a>
+        </div>           
     </div>
 </template>
