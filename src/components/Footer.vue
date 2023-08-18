8bf3ed09fabcbbf8712be6c1657c73f6552b27f6
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-08-19 03:35:55 +0700

    merapihkan footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,27 +1,56 @@
 <template>
-    <footer>
-        <div class="flex flex-row justify-between bg-[#464646] text-white p-10">
-            <div class="flex flex-col">
+    <!-- Boxicons -->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+    <footer class="w-full">
+        <div class="flex flex-row w-full justify-between bg-[#464646] text-white p-10">
+            <div class="flex flex-col basis-1/3">
                 <h2 class="font-semibold text-2xl mb-4">Tentang Website</h2>
-                <p>Website ini dibuat untuk seputar informasi tentang teknologi</p>
+                <p class="text-justify mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, id laboriosam. Ex rerum illo aspernatur cupiditate tenetur minima ea dolore, porro sit nesciunt officia dolorum aut sapiente maxime impedit cumque, aperiam deserunt exercitationem velit sint omnis. Deserunt dolore impedit amet vitae necessitatibus saepe illo est quae? Inventore quod placeat porro?</p>
                 <p class="text-end">- Developer</p>
             </div>
-            <div class="flex flex-col">
-                <h2 class="font-semibold text-2xl mb-4">Social Media</h2>
-                <a href="#">Ini Facebook</a>
-                <a href="#">Ini Instagram</a>
-                <a href="#">Ini Github</a>
-                <a href="#">Ini Linkedin</a>
+            <div class="basis-1/4">
+                <div class="ml-[25%]">
+                    <h2 class="font-semibold text-2xl mb-4">Social Media</h2>
+                    <div class="flex flex-col">
+                        <a href="#" class="flex items-center py-1">
+                            <i class='bx bxl-facebook text-white text-3xl bg-blue-700 border-[1px] border-black rounded p-1'></i>
+                            <span class="px-3">Follow us on Facebook</span>
+                        </a>
+                        <a href="#" class="flex items-center py-1">
+                            <i class='bx bxl-instagram text-white text-3xl bg-[#d6249f] border-[1px] border-black rounded p-1'></i>
+                            <span class="px-3">Follow us on Instagram</span>
+                        </a>
+                        <a href="#" class="flex items-center py-1">
+                            <i class='bx bxl-github text-white text-3xl bg-black border-[1px] border-black rounded p-1'></i>
+                            <span class="px-3">Follow us on Github</span>
+                        </a>
+                        <a href="#" class="flex items-center py-1">
+                            <i class='bx bxl-linkedin text-white text-3xl bg-blue-700 border-[1px] border-black rounded p-1'></i>
+                            <span class="px-3">Follow us on Linkedin</span>
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="flex flex-col">
-                <h2 class="font-semibold text-2xl mb-4">Kontak Informasi</h2>
-                <span>Alamat: Indonesia</span>
-                <span>0811-1111-1111</span>
-                <span>test@test.com</span>
+            <div class="basis-1/4">
+                <div class="flex flex-col ml-[40%]">
+                    <h2 class="font-semibold text-2xl mb-4">Kontak Informasi</h2>
+                    <span class="flex items-top py-1">
+                        <i class='bx bxs-home-alt-2 text-3xl'></i>
+                        <span class="px-3">Tech Insight,<br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, aut!</span>
+                    </span>
+                    <span class="flex items-center py-1">
+                        <i class='bx bx-phone text-3xl'></i>
+                        <span class="px-3">0811-1111-1111</span>
+                    </span>
+                    <span class="flex items-center py-1">
+                        <i class='bx bx-envelope text-3xl'></i>
+                        <span class="px-3">test@test.com</span>
+                    </span>
+                </div>
             </div>
         </div>
         <div class="mx-auto text-center py-4 bg-[#212121] text-white">
-            <p>Copyright 2023 Developer</p>
+            <p><i class='bx bx-copyright' ></i>Copyright 2023 Developer</p>
         </div>
     </footer>
 </template>
