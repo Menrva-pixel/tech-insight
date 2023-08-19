91e48ca8f96094e45a1b0ade52bdfd2020ad7865
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-08-19 16:53:12 +0700

    update bagian sidebar

--- src/components/CompanyInfo.vue
+++ src/components/CompanyInfo.vue
@@ -1,9 +1,8 @@
 <template>
-    <div id="company-profile" class="flex xl:flex-row flex-col mx-auto items-center justify-center xl:gap-8">
-        <img src="../assets/images/logo.png">
-        <div class="company-title flex flex-col gap-4">
+    <div id="company-profile" class="flex xl:flex-row flex-col mx-auto items-center justify-center xl:gap-8 px-[20%]">
+        <div class="company-title flex flex-col text-center gap-4 my-10">
             <h1 class="text-6xl">Tech Insight</h1>
-            <p class="text-2xl ml-2 first-letter:4xl">Perusahaan apa ini orang danta</p>
+            <p class="text-2xl first-letter:4xl">Tech Insight adalah Media digital yang membahas tentang Teknologi, Multimedia dan Elektronika baik dalam bentuk Berita, Review atau Tutorial melalui berbagai platform Sosial Media. Kami bertujuan untuk membantu orang-orang agar dapat mamahami dan menggunakan teknologi dan peralatan masa kini dengan lebih baik lagi.</p>
         </div>
     </div>
 </template>
