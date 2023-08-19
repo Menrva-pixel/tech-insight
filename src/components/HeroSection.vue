bd6765a4801ea3847baa7a68706d9f1c586b782c
Herdyanto26 <71887357+Menrva-pixel@users.noreply.github.com>
2023-08-19 15:12:41 +0700

    hero patch

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,10 +1,11 @@
 <template>
-    <div id="hero-section" class="banner flex flex-row justify-around">
-        <div class="banner-title flex flex-col items-start xl:pl-20 py-44">
+    <div id="hero-section" class="banner flex flex-row justify-around items-center px-4">
+        <div class="banner-title flex flex-col items-start xl:pl-20 py-44 z-10">
             <h1 class="text-7xl font-black text-blue-300 mt-28 mb-2">Tech Insight</h1>
             <h3 class="text-2xl font-semibold text-gray-200">Tech dan Tutorial</h3>
+            <p class="2xl:text-xl">Dapatkan info menarik seputar dunia teknologi, tutorial, tips dan trik.</p>
             <a href="" class="p-2 border-2 rounded-md mt-5 hover:bg-pink-400 duration-300 font-semibold text-gray-200 hover:text-black">Start Browsing</a>
         </div>
-        <img src="../assets/images/hero-layer-2.png" alt="banner-img" class="w-1/2 h-auto">          
+        <img src="../assets/images/hero-layer-2.png" alt="banner-img" class="w-1/2 h-auto pl-12">          
     </div>
 </template>
